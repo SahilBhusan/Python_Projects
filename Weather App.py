f84cec3aa8c227722b94dbeab81f57590a36661a
SahilBhusan <sahilkgp12@gmail.com>
2023-07-17 18:10:29 +0530

    My Python Projects

---
+++ Weather App.py
@@ -0,0 +1,19 @@
+from thinter import *
+import thinter as tk
+from geopy.geocoders import Nominatim
+from thinter import ttk,messagebox
+from timezonefinder import TimezoneFinder
+from datetime import datetime
+import requests
+import pytz
+
+root=Tk()
+root.title("Weather App")
+root.geometry("900x500+300+200")
+root.resizable(False,False)
+
+#search box
+Se
+
+root.mainloop
+
